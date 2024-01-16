7484ea2723aa53d8ab0a5fea7f58822b152670e7
Goswamiamruta <amrutagoswami87@gmail.com>
2024-01-16 20:22:29 +0000

    Api Homework Week 18

---
+++ src/test/java/com/restful/booker/curdtest/UserCRUDTest.java
@@ -0,0 +1,4 @@
+package com.restful.booker.curdtest;
+
+public class UserCRUDTest {
+}
---
+++ src/test/java/com/restful/booker/testsuite/userGetBookingTest.java
@@ -0,0 +1,41 @@
+package com.restful.booker.testsuite;
+
+import com.restful.booker.testbase.TestBase;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class userGetBookingTest extends TestBase {
+    @Test
+    public void getAllBookingIdSuccessfully(){
+        Response response = given()
+                .when()
+                .get("/booking");
+                response.then().statusCode(200);
+                response.prettyPrint();
+
+
+    }
+    @Test
+    public void getSingleBookingIdSuccessfully(){
+        Response response = given()
+                .pathParam("id",1300)
+                .when()
+                .get("/booking/{id}");
+        response.then().statusCode(200);
+        response.prettyPrint();
+
+
+    }
+    @Test
+    public void getPingHealthCheck() {
+        Response response = given()
+                .when()
+                .get("https://restful-booker.herokuapp.com/ping");
+        response.then()
+                .statusCode(201);
+    }
+
+
+}
